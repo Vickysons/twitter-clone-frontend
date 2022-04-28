9e53602d78ec8086d266f74c9a30745c2ea4ebe9
vickysons <victorkalu806@gmail.com>
2022-04-28 22:00:34 +0100

    some edits

--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -6,59 +6,62 @@ import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydre
 import NotesIcon from "@mui/icons-material/Notes";
 import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import SettingsIcon from "@mui/icons-material/Settings";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Profile from "../profile/Profile";
 function Feeds() {
   return (
     <>
       <div className="Feed_container">
-        <div className="home_Header">
-          <div className="HomePage">
-            <h3>Home</h3>
+        <div className="full_container">
+          <div className="home_Header">
+            <div className="HomePage">
+              <h3>Home</h3>
+            </div>
+            <div className="homeIcon">
+              <AutoAwesomeIcon />
+            </div>
           </div>
-          <div className="homeIcon">
-            <AutoAwesomeIcon />
+          <div className="profile_bottom">
+            <div className="image_profile">
+              <Profile />
+            </div>
+            <div className="info_profile">
+              <textarea
+                placeholder="What's happening?"
+                name=""
+                id="textArea"
+                cols="30"
+                rows="0"
+              ></textarea>
+            </div>
+          </div>
+          <div className="icon_page">
+            <div className="main_icon">
+              <div className="original_icon">
+                <CropOriginalIcon />
+              </div>
+              <div className="daydreamIcon">
+                <SettingsSystemDaydreamIcon />
+              </div>
+              <div className="notesIcon">
+                <NotesIcon />
+              </div>
+              <div className="emojiIcon">
+                <SentimentDissatisfiedIcon />
+              </div>
+              <div className="access">
+                <AccessTimeIcon />
+              </div>
+              <div className="settings">
+                <LocationOnIcon />
+              </div>
+            </div>
+            <div className="tweet_page">
+              <button>Tweet</button>
+            </div>
           </div>
         </div>
-        <div className="profile_bottom">
-          <div className="image_profile">
-            <Profile />
-          </div>
-          <div className="info_profile">
-            <textarea
-              placeholder="What's happening?"
-              name=""
-              id="textArea"
-              cols="30"
-              rows="0"
-            ></textarea>
-          </div>
-        </div>
-        <div className="icon_page">
-          <div className="main_icon">
-            <div className="original_icon">
-              <CropOriginalIcon />
-            </div>
-            <div className="daydreamIcon">
-              <SettingsSystemDaydreamIcon />
-            </div>
-            <div className="notesIcon">
-              <NotesIcon />
-            </div>
-            <div className="emojiIcon">
-              <SentimentDissatisfiedIcon />
-            </div>
-            <div className="access">
-              <AccessTimeIcon />
-            </div>
-            <div className="settings">
-              <SettingsIcon />
-            </div>
-          </div>
-          <div className="tweet_page">
-            <button>Tweet</button>
-          </div>
-        </div>
+        <div className="section_two"></div>
       </div>
     </>
   );
